e2343ce9e99ff8b6cd343b240ddba394ec23128c
just_user <marin6670@gmail.com>
2022-11-03 14:08:29 +0900

    .

--- src/main/java/com/primitive/SmartFactoryServer/Controller/Test_Controller.java
+++ src/main/java/com/primitive/SmartFactoryServer/Controller/Test_Controller.java
@@ -1,5 +1,6 @@
 package com.primitive.SmartFactoryServer.Controller;
 
+import com.primitive.SmartFactoryServer.DB_Connection_Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("")
 public class Test_Controller {
-
-
     private final DB_Connection_Data key = DB_Connection_Data.getInstance();
     @GetMapping
     public String testController(){
-
         return "working";
     }
 }
