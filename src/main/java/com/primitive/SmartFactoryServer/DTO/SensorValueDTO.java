5bd2202e2cb60ee7e1e2dc54e1d68faf43aae517
maria2580 <marin6670@gmail.com>
2022-11-06 03:33:12 +0900

    DTO에서 불필요한 필드 제거, @Getter 추가

--- src/main/java/com/primitive/SmartFactoryServer/DTO/SensorValueDTO.java
+++ src/main/java/com/primitive/SmartFactoryServer/DTO/SensorValueDTO.java
@@ -1,24 +1,12 @@
 package com.primitive.SmartFactoryServer.DTO;
 
-import com.primitive.SmartFactoryServer.DAO.BaseTimeEntity;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-
-
+import lombok.Getter;
+@Getter
 public class SensorValueDTO {
-
-    Long index;
-    String userID;
     String sensorName;
     String sensorValue;
-    public SensorValueDTO(String userID, String sensorName, String sensorValue) {
-        this.userID = userID;
+    public SensorValueDTO(String sensorName, String sensorValue) {
         this.sensorName=sensorName;
         this.sensorValue = sensorValue;
     }
