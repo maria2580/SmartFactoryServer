037db7f1c525d8d8b85de2f18cec74bc45d2b02e
maria2580 <marin6670@gmail.com>
2022-11-06 03:27:16 +0900

    @Repository 설정

--- src/main/java/com/primitive/SmartFactoryServer/DAO/SensorValues/SensorValueRepository.java
+++ src/main/java/com/primitive/SmartFactoryServer/DAO/SensorValues/SensorValueRepository.java
@@ -1,6 +1,8 @@
 package com.primitive.SmartFactoryServer.DAO.SensorValues;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SensorValueRepository extends JpaRepository<SensorValueDAO,Long> {
 }
