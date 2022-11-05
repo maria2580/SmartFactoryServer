28a027e4d85db0abbb60c2ee42a89feca4eac1b1
maria2580 <marin6670@gmail.com>
2022-11-06 03:26:56 +0900

    @Repository 설정, userId값으로 user 찾는 메서드 선언

--- src/main/java/com/primitive/SmartFactoryServer/DAO/users/UsersRepository.java
+++ src/main/java/com/primitive/SmartFactoryServer/DAO/users/UsersRepository.java
@@ -1,6 +1,11 @@
 package com.primitive.SmartFactoryServer.DAO.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface UsersRepository extends JpaRepository<UsersDAO,Long> {
+    List<UsersDAO> findByUserId(String userId);
 }
