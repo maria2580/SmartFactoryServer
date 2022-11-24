8aa254346a84c5193f289f1339d032792502afee
just_user <marin6670@gmail.com>
2022-11-25 04:44:37 +0900

    기존 반환값을 숫자 체계로 전환하여 반환 코드 관리

--- src/main/java/com/primitive/SmartFactoryServer/Controller/Login_Controller.java
+++ src/main/java/com/primitive/SmartFactoryServer/Controller/Login_Controller.java
@@ -27,37 +27,47 @@ public class Login_Controller {
             flag= true;
         }//flag가 true라면 이미 존재하는 유저임
         if (flag){
-            return "sign_up_failed";
+            return LoginStatus.EXIST_ID+"";
         }
         else {
             UsersDAO usersDAO=new UsersDAO(ID,PW);
             usersRepository.save(usersDAO);
         }
 
-        return "sign_up_successful";
+        return LoginStatus.PERMITTED+"";
     }
 
     @PostMapping("login")
-    public String login_request(@RequestBody String ID, @RequestBody String PW, @RequestParam String env) {
+    public String login_request(@RequestBody String ID, @RequestBody String PW, @RequestParam int env) {
         List<UsersDAO> usersDAOList = usersRepository.findByUserId(ID);
         boolean flag=false;
         for(int i=0;i<usersDAOList.size();i++){
             flag=true;
         }//flag가 true라면 존재하는 유저
         if(flag&&usersDAOList.get(0).getPw().equals(PW)){
-            if(env.equals("Factory")){
+            if(env==Env.FACTORY){
                 usersDAOList.get(0).updateFactoryToken();
-                return usersDAOList.get(0).getFactoryToken();
-            } else if (env.equals("Client")) {
+                return LoginStatus.PERMITTED+"";
+
+            } else if (env==Env.CLIENT) {
                 usersDAOList.get(0).updateClientToken();
-                return usersDAOList.get(0).getClientToken();
+                return LoginStatus.PERMITTED+"";
             }
             else {
-                return "invalid Parameter in env";
+                return LoginStatus.DENIED+"";
             }
         }
         else {
-            return "invalid user or pw";
+            return LoginStatus.DENIED+"";
         }
     }
+}
+class LoginStatus{
+    public static final int PERMITTED=0;
+    public static final int DENIED=1;
+    public static final int EXIST_ID=2;
+}
+class Env{
+    public static final int FACTORY=0;
+    public static final int CLIENT=1;
 }
