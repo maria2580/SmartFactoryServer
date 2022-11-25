1957690f28943a43b9cb72e7a85ce2f498cf6fa8
just_user <marin6670@gmail.com>
2022-11-25 14:58:13 +0900

    센서값DTO 객체 내부에 기록된 시간 정보 추가

--- src/main/java/com/primitive/SmartFactoryServer/DTO/SensorValueDTO.java
+++ src/main/java/com/primitive/SmartFactoryServer/DTO/SensorValueDTO.java
@@ -2,12 +2,20 @@ package com.primitive.SmartFactoryServer.DTO;
 
 
 import lombok.Getter;
+import java.time.LocalDateTime;
+
 @Getter
 public class SensorValueDTO {
     String sensorName;
     String sensorValue;
-    public SensorValueDTO(String sensorName, String sensorValue) {
-        this.sensorName=sensorName;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public SensorValueDTO(String sensorName, String sensorValue, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.sensorName = sensorName;
         this.sensorValue = sensorValue;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
     }
+
 }
