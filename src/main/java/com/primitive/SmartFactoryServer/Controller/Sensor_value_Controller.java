cf8b9efd2ccd41d896e85a0a3c0fd3373d439dbb
just_user <marin6670@gmail.com>
2022-11-25 16:36:51 +0900

    센서값 송신 수신 컨트롤러 세부 구현 완료

--- src/main/java/com/primitive/SmartFactoryServer/Controller/Sensor_value_Controller.java
+++ src/main/java/com/primitive/SmartFactoryServer/Controller/Sensor_value_Controller.java
@@ -7,10 +7,12 @@ import com.primitive.SmartFactoryServer.DAO.users.UsersRepository;
 import com.primitive.SmartFactoryServer.DTO.SensorValueDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 @RequestMapping("")
@@ -40,27 +42,71 @@ public class Sensor_value_Controller {
 
 
     @GetMapping("sensors_value/{ID}/resent_one")
-    public SensorValueDTO[] get_sensor_value_resent_one(@RequestParam String ID, @RequestBody String token){
-        ArrayList<SensorValueDTO> sensorValueses= new ArrayList<>();
+    public SensorValueDTO[] get_sensor_value_resent_one(@RequestParam String ID, @RequestBody String token) throws JSONException {
+        ArrayList<SensorValueDTO> sensorValues= new ArrayList<>();
+        List<SensorValueDAO> sensorValueDTOList= sensorValueRepository.findAllByUser(usersRepository.findByUserId(ID).get(0));
 
+        JSONArray jsonArray= new JSONArray(sensorValueDTOList.get(sensorValueDTOList.size()-1).getSensorValues());
+        for(int i=0;i<sensorValueDTOList.size();i++){
+            sensorValues.add((SensorValueDTO)jsonArray.get(i));
+        }
 
-        return (SensorValueDTO[])sensorValueses.toArray();
+        return (SensorValueDTO[])sensorValues.toArray();
     }
     @GetMapping("sensors_value/{ID}")
-    public SensorValueDTO[][] get_sensor_value_by_period(@RequestParam String ID, @RequestBody String token, @RequestBody String from, @RequestBody String to){
-        ArrayList<SensorValueDTO> sensorValues_one= new ArrayList<>();
-        ArrayList<SensorValueDTO[]> sensorValues_period = new ArrayList<>();//이 어레이에 추가할 때는 one을 toArray()하여 add()호출
+    public SensorValueDTO[][] get_sensor_value_by_period(@PathVariable("ID") String ID, @RequestBody String token, @RequestParam("from") String from, @RequestParam("to") String to){
+        ArrayList<ArrayList<SensorValueDTO>> result=new ArrayList<>();
+        List<SensorValueDAO> sensorValueDTOList= sensorValueRepository.findAllByUser(usersRepository.findByUserId(ID).get(0));
+        JSONArray[] jsonArray=new JSONArray[sensorValueDTOList.size()];
+        try {
+            for(int i=0;i<sensorValueDTOList.size();i++){
+                jsonArray[i]= new JSONArray(sensorValueDTOList.get(i).getSensorValues());
+            }
+            for (int i=0;i<jsonArray.length;i++){
+                //i=0에 같은 시점 센서값 n개 만큼 반복
+                for(int j=0; j<jsonArray[i].length();j++){
+                    result.get(i).add((SensorValueDTO)jsonArray[i].get(j));
+                }
+            }
+        }catch (JSONException e){e.printStackTrace();}
+        ;//안드로이드에서 날짜 받아오는 함수 사용할 예정
 
-
-        return (SensorValueDTO[][])sensorValues_period.toArray();
+        ArrayList<ArrayList<SensorValueDTO>> finedResult=new ArrayList<>();
+        Calendar min=getCaleder(from);
+        Calendar max=getCaleder(to);
+        for(int i=0; i<result.size();i++) {
+            LocalDateTime time = result.get(i).get(0).getCreatedDate();
+            Calendar temp = new GregorianCalendar();
+            temp.set(time.getYear(), time.getMonthValue(), time.getDayOfMonth());
+            if (temp.after(min) & temp.before(max)) {
+                finedResult.add(result.get(i));
+            }
+        }
+        return (SensorValueDTO[][])finedResult.toArray();
     }
     @GetMapping ("sensors_value/{ID}/all")
-    public SensorValueDTO[][] get_sensor_value_all(@RequestParam String ID, @RequestBody String token){
-        ArrayList<SensorValueDTO> sensorValues_one= new ArrayList<>();
-        ArrayList<SensorValueDTO[]> sensorValues_ten= new ArrayList<>();//이 어레이에 추가할 때는 one을 toArray()하여 add()호출
+    public SensorValueDTO[][] get_sensor_value_all(@RequestParam String ID, @RequestBody String token)  {
+        ArrayList<ArrayList<SensorValueDTO>> result=new ArrayList<>();
+        List<SensorValueDAO> sensorValueDTOList= sensorValueRepository.findAllByUser(usersRepository.findByUserId(ID).get(0));
+        JSONArray[] jsonArray=new JSONArray[sensorValueDTOList.size()];
+        try {
+            for(int i=0;i<sensorValueDTOList.size();i++){
+                jsonArray[i]= new JSONArray(sensorValueDTOList.get(i).getSensorValues());
+            }
+            for (int i=0;i<jsonArray.length;i++){
+                //i=0에 같은 시점 센서값 n개 만큼 반복
+                for(int j=0; j<jsonArray[i].length();j++){
+                    result.get(i).add((SensorValueDTO)jsonArray[i].get(j));
+                }
+            }
+        }catch (JSONException e){e.printStackTrace();}
 
-
-        return (SensorValueDTO[][])sensorValues_ten.toArray();
+        return (SensorValueDTO[][])result.toArray();
     }
-
+    static Calendar getCaleder(String yyyymmdd){
+        int Year=Integer.parseInt(yyyymmdd)/10000;
+        int Month=Integer.parseInt(yyyymmdd)%(Year*10000);
+        int Day=Integer.parseInt(yyyymmdd)%(Year*10000+Month*100);
+        return new GregorianCalendar(Year,Month,Day);
+    }
 }
--- src/main/java/com/primitive/SmartFactoryServer/DAO/SensorValues/SensorValueRepository.java
+++ src/main/java/com/primitive/SmartFactoryServer/DAO/SensorValues/SensorValueRepository.java
@@ -1,8 +1,12 @@
 package com.primitive.SmartFactoryServer.DAO.SensorValues;
 
+import com.primitive.SmartFactoryServer.DAO.users.UsersDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SensorValueRepository extends JpaRepository<SensorValueDAO,Long> {
+    List<SensorValueDAO> findAllByUser(UsersDAO usersDAO);
 }
