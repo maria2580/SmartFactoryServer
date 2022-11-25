080629ff9dd7400eb24ed55ce25121176b13daa6
just_user <marin6670@gmail.com>
2022-11-25 14:58:49 +0900

    Uri 경로 변경

--- src/main/java/com/primitive/SmartFactoryServer/Controller/Sensor_value_Controller.java
+++ src/main/java/com/primitive/SmartFactoryServer/Controller/Sensor_value_Controller.java
@@ -20,7 +20,7 @@ public class Sensor_value_Controller {
     UsersRepository usersRepository;
     @Autowired
     SensorValueRepository sensorValueRepository;
-    @PostMapping("sensors")
+    @PostMapping("sensors_value")
     public String post_sensor_value(@RequestBody SensorValueDTO[] sensorValueDTOs, @RequestBody String ID, @RequestBody String loginToken){
         JSONObject jsonObject = new JSONObject();
         JSONArray sensorValuesArr = new JSONArray();
@@ -39,22 +39,22 @@ public class Sensor_value_Controller {
     }
 
 
-    @GetMapping("sensors/{ID}/resent_one")
+    @GetMapping("sensors_value/{ID}/resent_one")
     public SensorValueDTO[] get_sensor_value_resent_one(@RequestParam String ID, @RequestBody String token){
         ArrayList<SensorValueDTO> sensorValueses= new ArrayList<>();
 
 
         return (SensorValueDTO[])sensorValueses.toArray();
     }
-    @GetMapping("sensors/{ID}")
-    public SensorValueDTO[][] get_sensor_value_by_period(@RequestParam String ID, @RequestBody String token, @PathVariable String from, @PathVariable String to){
+    @GetMapping("sensors_value/{ID}")
+    public SensorValueDTO[][] get_sensor_value_by_period(@RequestParam String ID, @RequestBody String token, @RequestBody String from, @RequestBody String to){
         ArrayList<SensorValueDTO> sensorValues_one= new ArrayList<>();
         ArrayList<SensorValueDTO[]> sensorValues_period = new ArrayList<>();//이 어레이에 추가할 때는 one을 toArray()하여 add()호출
 
 
         return (SensorValueDTO[][])sensorValues_period.toArray();
     }
-    @GetMapping ("sensors/{ID}/all")
+    @GetMapping ("sensors_value/{ID}/all")
     public SensorValueDTO[][] get_sensor_value_all(@RequestParam String ID, @RequestBody String token){
         ArrayList<SensorValueDTO> sensorValues_one= new ArrayList<>();
         ArrayList<SensorValueDTO[]> sensorValues_ten= new ArrayList<>();//이 어레이에 추가할 때는 one을 toArray()하여 add()호출
